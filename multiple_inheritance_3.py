dcfaec84915dc300e67aca6732638642b8d4e715
Mayukh Saha Roy <78737167+MayukhSahaRoy@users.noreply.github.com>
2024-01-04 16:08:49 +0530

    Create multiple_inheritance_3.py

---
+++ multiple_inheritance_3.py
@@ -0,0 +1,21 @@
+class Class1:
+    def disp(self):
+        print("In Class1")
+        super().disp()
+ 
+class Class2:
+    def disp(self):
+        print("In Class2")
+ 
+class Class3(Class1, Class2):
+    def disp(self):
+        print("In Class3")   
+        super().disp()
+      
+obj = Class3()
+obj.disp()
+
+#Output:
+#In Class3
+#In Class1
+#In Class2
